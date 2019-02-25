05a46ee502fb3533db28e259e92a41f2248a061d
DHINGRA,GD GOLDY <rl107147@rlas-macair-25.local>
2019-02-25 12:01:03 -0600

    Initial Commit

---
+++ bitzigram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitzigram
+//
+//  Created by DHINGRA,GD GOLDY on 2/25/19.
+//  Copyright © 2019 DHINGRA,GD GOLDY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
